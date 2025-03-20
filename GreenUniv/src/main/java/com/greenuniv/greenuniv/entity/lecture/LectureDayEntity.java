251b0b0ada9a26b7e4229e53727b11ff05b42fa3
id3ntity99 <jas06113@gmail.com>
2025-03-20 17:11:25 +0900

    강의 DTO 및 Entity 구현

---
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/lecture/LectureDayEntity.java
@@ -0,0 +1,62 @@
+package com.greenuniv.greenuniv.entity.lecture;
+
+/*
+ * 작성자: id3ntity99(이현민)
+ * 설명 : 강의시간 엔티티.
+ */
+
+import com.greenuniv.greenuniv.dto.lecture.LectureDayDTO;
+import com.greenuniv.greenuniv.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@Builder
+@Table(name = "lecture_day")
+@AllArgsConstructor
+@NoArgsConstructor
+public class LectureDayEntity implements BaseEntity {
+
+  @Id
+  private int id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "lecture_id")
+  LectureEntity lecture;
+
+  @Column(name = "day")
+  private String day;
+
+  @Override
+  public LectureDayDTO toDTO() {
+    return LectureDayDTO
+        .builder()
+        .id(id)
+        .day(day)
+        .build();
+  }
+
+  public static class LectureDayEntityBuilder {
+
+    public LectureDayEntity build() throws IllegalArgumentException {
+      boolean isLegal = Arrays.asList(LectureDayDTO.WEEK_DAYS).contains(day);
+      if (!isLegal) {
+        throw new IllegalArgumentException("유효하지 않은 요일: " + day);
+      }
+      return new LectureDayEntity(id, lecture, day);
+    }
+  }
+}
