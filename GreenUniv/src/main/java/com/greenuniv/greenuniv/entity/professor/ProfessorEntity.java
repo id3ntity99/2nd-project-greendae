b38e6ec746b6964240703d12c16f6515aec4671d
id3ntity99 <jas06113@gmail.com>
2025-03-22 18:10:51 +0900

    교수 DTO/Entity 수정

--- GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/professor/ProfessorEntity.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/professor/ProfessorEntity.java
@@ -12,6 +12,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,15 +40,64 @@ public class ProfessorEntity implements BaseEntity {
   @JoinColumn(name = "department_id")
   private DepartmentEntity department;
 
+  @Column(name = "graduated_from")
+  private String graduatedFrom;
+
+  @Column(name = "graduated_at")
+  private LocalDate graduatedAt;
+
+  @Column(name = "major")
+  private String major;
+
+  @Column(name = "degree")
+  private String degree;
+
+  @Column(name = "employed_at")
+  private LocalDate employedAt;
+
+  @Column(name = "status")
+  private String status;
+
+  @Column(name = "position")
+  private String position;
+
   @Column(name = "is_chief")
   private boolean isChief;
+
 
   @Override
   public ProfessorDTO toDTO() {
     return ProfessorDTO.builder()
         .id(id)
-        .department(department.toDTO())
         .user(user.toDTO())
+        .department(department.toDTO())
+        .graduatedAt(graduatedAt)
+        .graduatedFrom(graduatedFrom)
+        .major(major)
+        .degree(degree)
+        .employedAt(employedAt)
+        .status(status)
+        .position(position)
+        .isChief(isChief)
         .build();
+  }
+
+  public static class ProfessorEntityBuilder {
+
+    public ProfessorEntity build() throws IllegalArgumentException {
+      boolean isDegreeLegal = Arrays.asList(ProfessorDTO.DEGREES).contains(degree);
+      boolean isStatusLegal = Arrays.asList(ProfessorDTO.STATUS).contains(status);
+      boolean isPositionLegal = Arrays.asList(ProfessorDTO.POSITIONS).contains(position);
+
+      if (!isDegreeLegal) {
+        throw new IllegalArgumentException("유효하지 않은 교수 학위: " + degree);
+      } else if (!isStatusLegal) {
+        throw new IllegalArgumentException("유효하지 않은 교수 상태 : " + status);
+      } else if (!isPositionLegal) {
+        throw new IllegalArgumentException("유효하지 않은 교수 직위: " + position);
+      }
+      return new ProfessorEntity(id, user, department, graduatedFrom, graduatedAt, major, degree,
+          employedAt, status, position, isChief);
+    }
   }
 }
