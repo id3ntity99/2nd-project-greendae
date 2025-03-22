e50f398325e5e67ceb81a526b0415435930c8a1b
id3ntity99 <jas06113@gmail.com>
2025-03-22 20:22:50 +0900

    사용자 DTO 및 Entity 구현

--- GreenUniv/src/main/java/com/greenuniv/greenuniv/dto/user/UserDTO.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/dto/user/UserDTO.java
@@ -2,10 +2,13 @@ package com.greenuniv.greenuniv.dto.user;
 
 import com.greenuniv.greenuniv.dto.BaseDTO;
 import com.greenuniv.greenuniv.entity.user.UserEntity;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @Builder
@@ -13,14 +16,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO implements BaseDTO {
 
+  public static final String GENDER_MALE = "m";
+  public static final String GENDER_FEMALE = "f";
+  public static final String[] GENDERS = {GENDER_MALE, GENDER_FEMALE};
+
+  public static final String ROLE_PROF = "professor";
+  public static final String ROLE_STUDENT = "student";
+  public static final String ROLE_ADMIN = "admin";
+  public static final String[] ROLES = {ROLE_PROF, ROLE_STUDENT, ROLE_ADMIN};
+
   private String id;
   private String password;
   private String name;
+  private String engName;
+  private String gender;
+  private String nationality;
   private String socialNumber;
   private String email;
   private String contact;
+  private String zip;
+  private String address;
+  private String addressDetail;
   private String role;
-  private boolean agreed;
+  private boolean agreedTerms;
+  @CreationTimestamp
+  private LocalDateTime registerDate;
+  private LocalDateTime leaveDate;
 
   @Override
   public UserEntity toEntity() {
@@ -28,10 +49,37 @@ public class UserDTO implements BaseDTO {
         .id(id)
         .password(password)
         .name(name)
+        .engName(engName)
+        .gender(gender)
+        .nationality(nationality)
         .socialNumber(socialNumber)
         .email(email)
         .contact(contact)
+        .zip(zip)
+        .address(address)
+        .addressDetail(addressDetail)
         .role(role)
+        .agreedTerms(agreedTerms)
+        .registerDate(registerDate)
+        .leaveDate(leaveDate)
         .build();
+  }
+
+  public static class UserDTOBuilder {
+
+    public UserDTO build() throws IllegalArgumentException {
+      boolean isGenderLegal = Arrays.asList(GENDERS).contains(gender);
+      boolean isRoleLegal = Arrays.asList(ROLES).contains(role);
+
+      if (!isGenderLegal) {
+        String message = String.format("유효하지 않은 성별([%s]): %s", Arrays.toString(GENDERS), gender);
+        throw new IllegalArgumentException(message);
+      } else if (!isRoleLegal) {
+        String message = String.format("유효하지 않은 사용자 유형([%s]): %s", Arrays.toString(ROLES), role);
+        throw new IllegalArgumentException(message);
+      }
+      return new UserDTO(id, password, name, engName, gender, nationality, socialNumber, email,
+          contact, zip, address, addressDetail, role, agreedTerms, registerDate, leaveDate);
+    }
   }
 }
--- GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/user/UserEntity.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/user/UserEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +24,7 @@ import org.hibernate.annotations.CreationTimestamp;
 @NoArgsConstructor
 public class UserEntity implements BaseEntity {
 
+  //TODO: Impl this
   @Id
   private String id;
 
@@ -31,6 +33,10 @@ public class UserEntity implements BaseEntity {
 
   @Column(name = "name")
   private String name;
+
+  private String engName;
+  private String gender;
+  private String nationality;
 
   @Column(name = "social_number")
   private String socialNumber;
@@ -41,8 +47,19 @@ public class UserEntity implements BaseEntity {
   @Column(name = "contact")
   private String contact;
 
+  @Column(name = "zip")
+  private String zip;
+
+  @Column(name = "address")
+  private String address;
+
+  @Column(name = "address_detail")
+  private String addressDetail;
+
   @Column(name = "role")
   private String role;
+
+  @Column(name = "agreed_terms")
   private boolean agreedTerms;
 
   @Column(name = "register_date")
@@ -58,10 +75,41 @@ public class UserEntity implements BaseEntity {
         .id(id)
         .password(password)
         .name(name)
+        .engName(engName)
+        .gender(gender)
+        .nationality(nationality)
         .socialNumber(socialNumber)
         .email(email)
         .contact(contact)
+        .zip(zip)
+        .address(address)
+        .addressDetail(addressDetail)
         .role(role)
+        .agreedTerms(agreedTerms)
+        .registerDate(registerDate)
+        .leaveDate(leaveDate)
         .build();
+  }
+
+  public static class UserEntityBuilder {
+
+    public UserEntity build() throws IllegalArgumentException {
+
+      boolean isGenderLegal = Arrays.asList(UserDTO.GENDERS).contains(gender);
+      boolean isRoleLegal = Arrays.asList(UserDTO.ROLES).contains(role);
+
+      if (!isGenderLegal) {
+        String message = String.format("유효하지 않은 성별([%s]): %s", Arrays.toString(UserDTO.GENDERS),
+            gender);
+        throw new IllegalArgumentException(message);
+      } else if (!isRoleLegal) {
+        String message = String.format("유효하지 않은 사용자 유형([%s]): %s", Arrays.toString(UserDTO.ROLES),
+            role);
+        throw new IllegalArgumentException(message);
+      }
+      return new UserEntity(id, password, name, engName, gender, nationality, socialNumber, email,
+          contact, zip, address, addressDetail, role, agreedTerms, registerDate, leaveDate);
+    }
+
   }
 }
