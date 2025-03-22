4e8ce4bd48cf7cb2faa4c101c648bf38d3e01aff
id3ntity99 <jas06113@gmail.com>
2025-03-22 20:08:24 +0900

    이용약관 DTO 및 Entity 구현

---
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/entity/term/TermEntity.java
@@ -0,0 +1,41 @@
+package com.greenuniv.greenuniv.entity.term;
+
+import com.greenuniv.greenuniv.dto.term.TermDTO;
+import com.greenuniv.greenuniv.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@Table(name = "term")
+@AllArgsConstructor
+@NoArgsConstructor
+public class TermEntity implements BaseEntity {
+
+  @Id
+  private int id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "content")
+  private String content;
+
+  @Override
+  public TermDTO toDTO() {
+    return TermDTO.builder()
+        .id(id)
+        .title(title)
+        .content(content)
+        .build();
+  }
+}
