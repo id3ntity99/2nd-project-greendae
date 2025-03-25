7d9ecf1f33f046fb4b4d1bc4ac98e820aa5bc837
id3ntity99 <jas06113@gmail.com>
2025-03-25 19:56:31 +0900

    StudentService bean 등록

--- GreenUniv/src/main/java/com/greenuniv/greenuniv/config/AppConfig.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.greenuniv.greenuniv.config;
 
 import com.greenuniv.greenuniv.dao.mapper.GenericMapper;
 import com.greenuniv.greenuniv.dto.registry.RegistryLectureDTO;
+import com.greenuniv.greenuniv.dto.student.StudentDTO;
 import com.greenuniv.greenuniv.service.DefaultGenericService;
 import com.greenuniv.greenuniv.service.GenericService;
 import org.mybatis.spring.annotation.MapperScan;
@@ -17,6 +18,12 @@ public class AppConfig {
   @Bean
   public GenericService<RegistryLectureDTO, String> registryService(
       GenericMapper<RegistryLectureDTO, String> strategy) {
+    return new DefaultGenericService<>(strategy);
+  }
+
+  @Bean
+  public GenericService<StudentDTO, String> studentService(
+      GenericMapper<StudentDTO, String> strategy) {
     return new DefaultGenericService<>(strategy);
   }
 }
