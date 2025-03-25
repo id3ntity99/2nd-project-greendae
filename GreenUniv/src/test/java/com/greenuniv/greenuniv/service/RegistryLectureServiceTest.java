ea8a4c3a1c62fac41de0bc57d962a19b9aeb05c9
id3ntity99 <jas06113@gmail.com>
2025-03-25 18:57:31 +0900

    RegistryLectureService 테스트

---
+++ GreenUniv/src/test/java/com/greenuniv/greenuniv/service/RegistryLectureServiceTest.java
@@ -0,0 +1,54 @@
+package com.greenuniv.greenuniv.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.greenuniv.greenuniv.dao.mapper.GenericMapper;
+import com.greenuniv.greenuniv.dto.registry.RegistryLectureDTO;
+import com.greenuniv.greenuniv.dto.user.UserDTO;
+import java.util.List;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+class RegistryLectureServiceTest {
+
+  @Autowired
+  private GenericService<RegistryLectureDTO, String> service;
+
+  @Autowired
+  private GenericMapper<UserDTO, String> userMapper;
+
+  private void assertLecture(RegistryLectureDTO lecture) {
+    assertNotNull(lecture.getLecture().getDepartment().getName());
+    assertNotNull(lecture.getLecture().getClassification());
+    assertNotEquals(0, lecture.getLecture().getLevel());
+    assertNotNull(lecture.getLecture().getId());
+    assertNotNull(lecture.getLecture().getName());
+    assertNotEquals(0, lecture.getLecture().getCredit());
+    assertNotNull(lecture.getLecture().getProfessor().getUser().getName());
+    assertEquals(0, lecture.getRegisteredNumber());
+    assertNotEquals(0, lecture.getMaxRegistered());
+
+  }
+
+  @Test
+  @Order(2)
+  void testSelectAllRegistryLectures_success() {
+    List<RegistryLectureDTO> lectures = service.findAll();
+    lectures.forEach(this::assertLecture);
+  }
+
+  @Test
+  @Order(1)
+  void testSelectRegistryLectureById_success() {
+    RegistryLectureDTO lecture = service.findById("101027");
+    assertLecture(lecture);
+  }
+}
