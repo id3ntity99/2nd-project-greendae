417f70cbf145b15f82c410ff1e50bbb5dd70dd1c
jaeyeong <jaeyoung0560@gmail.com>
2025-03-28 11:34:39 +0900

    헤더 매핑 진행중

--- GreenUniv/src/main/java/com/greenuniv/greenuniv/controller/HeaderController.java
+++ GreenUniv/src/main/java/com/greenuniv/greenuniv/controller/HeaderController.java
@@ -29,6 +29,56 @@ public class HeaderController {
     @GetMapping("/introduce/organization")
     public String organization(){return "/introduce/organization";}
 
+    @GetMapping("/introduce/advice")
+    public String advice(){return "/introduce/advice";}
+
+    @GetMapping("/university/humanities")
+    public String humanities(){return "/university/humanities";}
+
+    @GetMapping("/university/naturalScience")
+    public String naturalScience(){return "/university/naturalScience";}
+
+    @GetMapping("/university/engineering")
+    public String engineering(){return "/university/engineering";}
+
+    @GetMapping("/university/education")
+    public String education(){return "/university/education";}
+
+    @GetMapping("/university/graduate")
+    public String graduate(){return "/university/graduate";}
+
+    @GetMapping("/academicInformation/notice")
+    public String notice(){return "/academicInformation/notice";}
+
+    @GetMapping("/academicInformation/schedule")
+    public String schedule(){return "/academicInformation/schedule";}
+
+    @GetMapping("/academicInformation/registration")
+    public String registration(){return "/academicInformation/registration";}
+
+    @GetMapping("/academicInformation/score")
+    public String score(){return "/academicInformation/score";}
+
+    @GetMapping("/academicInformation/graduation")
+    public String graduation(){return "/academicInformation/graduation";}
+
+    @GetMapping("/academicInformation/qna")
+    public String qna(){return "/academicInformation/qna";}
+
+    @GetMapping("/collegeLife/club")
+    public String club(){return "/collegeLife/club";}
+
+    @GetMapping("/collegeLife/diet")
+    public String diet(){return "/collegeLife/diet";}
+
+    @GetMapping("/collegeLife/council")
+    public String council(){return "/collegeLife/council";}
+
+    @GetMapping("/collegeLife/gallery")
+    public String gallery(){return "/collegeLife/gallery";}
+
+
+
 
 }
 
